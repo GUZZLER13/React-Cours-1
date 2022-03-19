9c8aeb1ad2cc97ac480b122ef8f9400b36a8d19a
guzzler13 <stephanedasil@gmail.com>
2022-03-19 10:52:11 +0100

    add font and img

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navigation from './components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto nihil et officiis quam tempore, inventore enim? Vel dolorem hic voluptate temporibus accusamus repellat maiores deleniti, modi distinctio quisquam eius quia magnam assumenda rerum inventore mollitia fugit iusto in aliquid impedit. Aperiam vero vitae quo maiores libero nam, excepturi eum quidem sapiente natus doloribus possimus nobis impedit. Aperiam voluptatibus tenetur quasi!</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, minima vero? Voluptate tempora eaque laudantium tenetur quae blanditiis eos id obcaecati. Temporibus eius, doloremque voluptatem aut dolore odit? Excepturi cumque dolorum aliquid deserunt obcaecati. Architecto enim perferendis libero autem quod quia, recusandae numquam adipisci repellat quis optio minus vero omnis ea facilis sunt explicabo, delectus fuga fugiat, aliquam natus aspernatur. Minus, debitis molestias ad blanditiis minima doloribus molestiae quidem atque ea ipsam deleniti sint voluptatum? In possimus nobis doloremque. Quasi esse expedita numquam sed sapiente, quae, magni ex atque quos, assumenda quaerat quis? Magni reprehenderit sapiente delectus in sed assumenda molestias dolores nihil minima dicta, nisi quo nulla id quaerat, architecto magnam eveniet repellendus dolor commodi? Nostrum quibusdam vitae fugit eveniet cupiditate mollitia nihil perspiciatis excepturi quae hic, quia reiciendis praesentium, eius dolor modi? Modi recusandae nihil inventore tenetur praesentium.</p>
+
+        </div>
+    );
+};
+
+export default About;
